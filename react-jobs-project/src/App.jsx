857e64a7806cf137b441e3bcd5936e92711bc785
zahrabateninia <zahrabatenin@gmail.com>
2024-08-26 16:12:09 +0330

    Add jobs.json file in src folder

--- react-jobs-project/src/App.jsx
+++ react-jobs-project/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import HomeCards from './components/HomeCards'
+import Card from './components/Card'
 
 const App = () => {
   return (
     <>
     <Navbar />
     <Hero />
+    <HomeCards />
     </>
   )
 }
---
+++ react-jobs-project/src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import logo from '../assets/images/logo.svg';
+import React from 'react'
+
+const Navbar = () => {
+    return (    
+    <nav className='bg-indigo-700 border-b border-indigo-500'>
+      <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
+        <div className='flex h-20 items-center justify-between'>
+          <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
+            <a 
+              className='flex flex-shrink-0 items-center mr-4' href='/index.html'
+            >
+              <img 
+                className='h-10 w-auto' 
+                src={logo} 
+                alt='React Jobs' 
+              />
+              <span className='hidden md:block text-white text-2xl font-bold ml-2'>
+                React Jobs
+              </span>
+            </a>
+            <div className='md:ml-auto'>
+              <div className='flex space-x-2'>
+                <a href='' >
+                  Home
+                </a>
+                <a href='' >
+                  Jobs
+                </a>
+                <a href=''>
+                  Add Job
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar
