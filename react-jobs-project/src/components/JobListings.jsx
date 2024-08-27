3dc486c97489a6ea2a202e1e92bdc83e943240fa
zahrabateninia <zahrabatenin@gmail.com>
2024-08-27 11:32:59 +0330

    Add the section for browsing jobs

--- react-jobs-project/src/components/JobListings.jsx
+++ react-jobs-project/src/components/JobListings.jsx
@@ -1,15 +1,23 @@
-import jobs from "../jobs.json"
-import JobListing from "./JobListing"
+import { useState} from 'react';
+import JobListing from './JobListing';
 
-import React from 'react'
 
-const JobListings = () => {
-  console.log(jobs)
+const JobListings = ({ isHome = false }) => {
+
+
   return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default JobListings
+    <section className='bg-blue-50 px-4 py-10'>
+      <div className='container-xl lg:container m-auto'>
+        <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
+          {isHome ? 'Recent Jobs' : 'Browse Jobs'}
+        </h2>
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {jobs.map((job) => (
+              <JobListing key={job.id} job={job} />
+            ))}
+          </div>
+      </div>
+    </section>
+  );
+};
+export default JobListings;
