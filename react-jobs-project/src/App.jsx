da9b7854eca5f80f8777fef136fb8e2784548dc2
zahrabateninia <zahrabatenin@gmail.com>
2024-08-27 11:12:53 +0330

    Add JobListing component

--- react-jobs-project/src/App.jsx
+++ react-jobs-project/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import HomeCards from './components/HomeCards'
-import Card from './components/Card'
+import JobListings from "./components/JobListings"
 
 const App = () => {
   return (
@@ -10,6 +10,7 @@ const App = () => {
     <Navbar />
     <Hero />
     <HomeCards />
+    <JobListings />
     </>
   )
 }
---
+++ react-jobs-project/src/components/JobListings.jsx
@@ -0,0 +1,15 @@
+import jobs from "../jobs.json"
+import JobListing from "./JobListing"
+
+import React from 'react'
+
+const JobListings = () => {
+  console.log(jobs)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default JobListings
