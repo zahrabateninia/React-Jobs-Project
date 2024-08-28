f5f428280a5ce238efd4956c6ebd733e22404754
zahrabateninia <zahrabatenin@gmail.com>
2024-08-28 14:28:24 +0330

    Import Link component and change all the href in Navbar into to

--- react-jobs-project/src/components/Navbar.jsx
+++ react-jobs-project/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import logo from '../assets/images/logo.svg';
 import React from 'react'
+// using a Link tag it doesn't do an actual refresh so we use it instead of a tag
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (    
@@ -7,8 +9,9 @@ const Navbar = () => {
       <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
         <div className='flex h-20 items-center justify-between'>
           <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
-            <a 
-              className='flex flex-shrink-0 items-center mr-4' href='/index.html'
+            <Link
+              className='flex flex-shrink-0 items-center mr-4'
+              to='/' // homepage
             >
               <img 
                 className='h-10 w-auto' 
@@ -18,18 +21,18 @@ const Navbar = () => {
               <span className='hidden md:block text-white text-2xl font-bold ml-2'>
                 React Jobs
               </span>
-            </a>
+            </Link>
             <div className='md:ml-auto'>
               <div className='flex space-x-2'>
-                <a href='/' >
+                <Link to='/' >
                   Home
-                </a>
-                <a href='/jobs' >
+                </Link>
+                <Link to='/jobs' >
                   Jobs
-                </a>
-                <a href='/add-job'>
+                </Link>
+                <Link to='/add-job'>
                   Add Job
-                </a>
+                </Link>
               </div>
             </div>
           </div>
