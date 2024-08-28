21a4b98394bc14c076041df6c097628554561d77
zahrabateninia <zahrabatenin@gmail.com>
2024-08-28 13:05:08 +0330

    Add the Navbar at the top of every page

--- react-jobs-project/src/layouts/MainLayout.jsx
+++ react-jobs-project/src/layouts/MainLayout.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 
 const MainLayout = () => {
   return (
     <>
+        <Navbar />     
         <Outlet />
     </>
   )
