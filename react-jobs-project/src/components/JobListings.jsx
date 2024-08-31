51a4dc25356f99386018b55fe912a645769e24dd
zahrabateninia <zahrabatenin@gmail.com>
2024-08-31 21:11:57 +0330

    Display Loading... when the data is loading, else show the jobs

--- react-jobs-project/src/components/JobListings.jsx
+++ react-jobs-project/src/components/JobListings.jsx
@@ -22,7 +22,7 @@ const JobListings = ({ isHome = false }) => {
 
     }
   })
-
+ 
   
   return (
     <section className='bg-blue-50 px-4 py-10'>
@@ -31,9 +31,13 @@ const JobListings = ({ isHome = false }) => {
           {isHome ? 'Recent Jobs' : 'Browse Jobs'}
         </h2>
           <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-            {jobs.map((job) => (
+            { loading ? (<h2>Loading...</h2>) : (
+              <>
+              {jobs.map((job) => (
               <JobListing key={job.id} job={job} />
             ))}
+              </>
+            )}
           </div>
       </div>
     </section>
