a49cc668639915b8e92bb46b6c4710f19646c2a9
zahrabateninia <zahrabatenin@gmail.com>
2024-09-01 08:34:00 +0330

    Import Spinner in JobListings component to show ClipSpinner while fetching data

--- react-jobs-project/src/components/JobListings.jsx
+++ react-jobs-project/src/components/JobListings.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect} from 'react';
 import JobListing from './JobListing';
 // npm i react-spinners in order to use the package:
-import 
+import Spinner from "./Spinner"
 
 const JobListings = ({ isHome = false }) => {
   const [jobs, setJobs] = useState([])
@@ -27,20 +27,21 @@ const JobListings = ({ isHome = false }) => {
   
   return (
     <section className='bg-blue-50 px-4 py-10'>
-      <div className='container-xl lg:container m-auto'>
-        <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
-          {isHome ? 'Recent Jobs' : 'Browse Jobs'}
-        </h2>
-          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-            { loading ? (<h2>Loading...</h2>) : (
-              <>
-              {jobs.map((job) => (
-              <JobListing key={job.id} job={job} />
-            ))}
-              </>
+          <div className='container-xl lg:container m-auto'>
+            <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
+              {isHome ? 'Recent Jobs' : 'Browse Jobs'}
+            </h2>
+
+            {loading ? (
+              <Spinner loading={loading} />
+            ) : (
+              <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+                {jobs.map((job) => (
+                  <JobListing key={job.id} job={job} />
+                ))}
+              </div>
             )}
           </div>
-      </div>
     </section>
   );
 };
