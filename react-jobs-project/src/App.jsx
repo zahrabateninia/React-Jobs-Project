2f50fa1d62b4bdf1c09fae84f55ec6b3c75fe982
zahrabateninia <zahrabatenin@gmail.com>
2024-09-02 16:10:33 +0330

    Use useLoaderData hook from react router to fetch data instead of using useEffect

--- react-jobs-project/src/App.jsx
+++ react-jobs-project/src/App.jsx
@@ -9,7 +9,7 @@ import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
-import JobPage from "./pages/JobPage"
+import JobPage, {jobLoader} from "./pages/JobPage"
 
 const App = () => {
     const router = createBrowserRouter(
@@ -17,7 +17,7 @@ const App = () => {
         <Route path='/' element={<MainLayout />}>
           <Route index element={<HomePage />} />
           <Route path='/jobs' element={<JobsPage />} />
-          <Route path='/jobs/:id' element={<JobPage />} />
+          <Route path='/jobs/:id' element={<JobPage />} loader= {jobLoader} />
           <Route path='/*' element={<NotFoundPage />} />
         </Route>
     )
--- react-jobs-project/src/pages/JobPage.jsx
+++ react-jobs-project/src/pages/JobPage.jsx
@@ -1,30 +1,43 @@
 import React from 'react'
-import {useEffect, useState} from 'react'
-import { useParams} from 'react-router-dom'
+// import {useEffect, useState} from 'react'
+import { useParams, useLoaderData } from 'react-router-dom'
 import Spinner from '../components/Spinner'
 
 const JobPage = () => {
   const { id } = useParams()
-  const [job, setJob] = useState(null)
-  const [loading, setLoading] = useState(true)
+  const job = useLoaderData()
 
-  useEffect(() => {
-    const fetchJob = async () =>{
-        try {
-            const res = await fetch(`/api/jobs/${id}`);
-            const data = await res.json();
-            console.log(data)
-            setJob(data);
-          } catch (error) {
-            console.log('Error fetching data', error);
-          } finally {
-            setLoading(false);
-          }
-    }
-    fetchJob()
-  }, [])
-  return loading ? <Spinner /> : 
-  <h1> { job.title }</h1>
-}
+// Fetching Data Using useEffect: 
 
-export default JobPage
+//   const [job, setJob] = useState(null)
+//   const [loading, setLoading] = useState(true)
+
+//   useEffect(() => {
+//     const fetchJob = async () =>{
+//         try {
+//             const res = await fetch(`/api/jobs/${id}`);
+//             const data = await res.json();
+//             console.log(data)
+//             setJob(data);
+//           } catch (error) {
+//             console.log('Error fetching data', error);
+//           } finally {
+//             setLoading(false);
+//           }
+//     }
+//     fetchJob()
+//   }, [])
+   return <h1> { job.title }</h1>
+ };
+
+
+// Fetching Data using Data Loader from React Router
+
+const jobLoader = async ({ params })=> {
+    const res =  await fetch(`/api/jobs/${ params.id }`)
+    const data = res.json()
+    return data
+};
+
+
+export { JobPage as default, jobLoader }
