90f9f3f0c9cdf0b1a7c29bc171f1d716e952c9d8
zahrabateninia <zahrabatenin@gmail.com>
2024-09-02 15:39:17 +0330

    Import useParams hook to get the id of each job and diplay its content on its own page

--- react-jobs-project/src/pages/JobPage.jsx
+++ react-jobs-project/src/pages/JobPage.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
+import {useEffect, useState} from 'react'
+import { useParams} from 'react-router-dom'
 
 const JobPage = () => {
+  const { id } = useParams()
+  const [job, setJob] = useState(null)
+
+  useEffect(() => {
+    const fetchJob = async () =>{
+        try {
+            const res = await fetch(`/api/job/${id}`);
+            const data = await res.json();
+            setJob(data);
+          } catch (error) {
+            console.log('Error fetching data', error);
+          } finally {
+            setLoading(false);
+          }
+    }
+  }, [])
   return (
     <div>
       
